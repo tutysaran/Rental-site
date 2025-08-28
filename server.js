e0e445154cd8681bad8630d2ef8b231bf43fda3a
saran <selvakumarraj432@gmail.com>
2025-08-28 12:07:37 +0530

    second  commit

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Error:", err));
+
 // Routes
+app.get("/", (req, res) => {
+  res.send("Rental API is running ✅");
+});
+
 app.use("/api/products", productRoutes);
 
-// Connect MongoDB + Start Server
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
